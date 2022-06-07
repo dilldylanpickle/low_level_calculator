c4a00e30e35923cd9a9a535a49025aabedae6c9d
dilldylanpickle <dgonzales4055@gmail.com>
2022-06-07 18:22:56 -0500

    fixed user input and output

--- calculator.asm
+++ calculator.asm
@@ -1,2 +1,52 @@
 section .data
-        text1db "Enter an integer: "
+        text1 db "Enter an integer: "
+        text2 db "You have entered "
+
+section .bss
+        integer resb 16
+
+section .text
+        global _start
+_start:
+
+        call _printText1
+        call _getInteger
+        call _printText2
+        call _printInteger
+
+        mov rax, 69
+        mov rdi, 0
+        syscall
+        ret
+
+_getInteger:
+        mov rax, 0
+        mov rdi, 0
+        mov rsi, integer
+        mov rdx, 16
+        syscall
+        ret
+
+_printText1:
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, text1
+        mov rdx, 18
+        syscall
+        ret
+
+_printText2:
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, text2
+        mov rdx, 17
+        syscall
+        ret
+
+_printInteger:
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, integer
+        mov rdx, 16
+        syscall
+        ret
